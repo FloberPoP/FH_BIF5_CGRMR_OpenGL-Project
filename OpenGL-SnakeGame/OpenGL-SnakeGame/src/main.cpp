f584af1e4cf78ebadd57399768df646d8a277a14
Dominik Neuwirth <domineu04@gmail.com>
2025-01-23 18:12:57 +0100

    Texture geht

--- OpenGL-SnakeGame/OpenGL-SnakeGame/src/main.cpp
+++ OpenGL-SnakeGame/OpenGL-SnakeGame/src/main.cpp
@@ -168,8 +168,6 @@ int main(int argc, char** argv)
     initShaders();
     //initGL();
 
-    snakeTexture = loadTexture("assets/robo.png");
-
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -632,15 +630,16 @@ void drawSnake()
     if (score > 0)
     {
         drawSnakeTail(snakeBodyColor);
+        snakeTexture = loadTexture("assets/robo.png");
         for (SnakePart* sp = snake->GetTail()->prev; sp->prev != nullptr; sp = sp->prev)
         {
-            //drawQuad(sp->pos.x, sp->pos.y, SNAKE_SIZE, SNAKE_SIZE, snakeBodyColor);
+            drawQuad(sp->pos.x, sp->pos.y, SNAKE_SIZE, SNAKE_SIZE, snakeBodyColor);
 
+            
             glEnable(GL_BLEND);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            glDisable(GL_DEPTH_TEST);  // Disable depth testing
             drawTexturedQuad(sp->pos.x, sp->pos.y, SNAKE_SIZE, SNAKE_SIZE, snakeTexture);
-            glEnable(GL_DEPTH_TEST);   // Re-enable depth testing
+            
         }
     }
     drawSnakeHead(snakeHeadColor);
@@ -682,43 +681,20 @@ GLuint loadTexture(const char* filePath) {
 }
 
 void drawTexturedQuad(float x, float y, float w, float h, GLuint texture) {
-    glUseProgram(shaderProgram);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    //glColor3d(1, 0, 0);
 
-    float vertices[] = {
-        // Positions          // Texture Coords
-        x - w / 2, y - h / 2,  0.0f, 0.0f, // Bottom-left
-        x + w / 2, y - h / 2,  1.0f, 0.0f, // Bottom-right
-        x + w / 2, y + h / 2,  1.0f, 1.0f, // Top-right
-        x - w / 2, y + h / 2,  0.0f, 1.0f  // Top-left
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glBindVertexArray(0);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(x - w / 2, y - h / 2); // bottom left
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex2f(x + w / 2, y - h / 2); // bottom right
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex2f(x + w / 2, y + h / 2); // top right
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex2f(x - w / 2, y + h / 2); // top left
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
 }
 
 void drawSnakeBody() {
