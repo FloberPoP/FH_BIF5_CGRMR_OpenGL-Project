44b67488a1fdfdef67e8e723665f359a896eb992
Dominik Neuwirth <domineu04@gmail.com>
2025-01-23 21:28:22 +0100

    Build

--- OpenGL-SnakeGame/OpenGL-SnakeGame/src/main.cpp
+++ OpenGL-SnakeGame/OpenGL-SnakeGame/src/main.cpp
@@ -147,6 +147,8 @@ float snakeHeadColor[] = { 0.0f, 1.0f, 0.2f, 1.0f };
 
 int main(int argc, char** argv)
 {
+    srand(time(0));
+
     Grid GRID = Grid();
     snake = new Snake();
     fruits.push_back(Fruit());
